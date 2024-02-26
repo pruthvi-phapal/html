1135d5f41eac100861a6e77ceaa8f9b35df88152
pruthvi-phapal <pruthviphapal05@gmail.com>
2024-02-26 17:09:14 +0530

    31 commit

---
+++ 31.full screen navigation image/app.js
@@ -0,0 +1,15 @@
+const menu = document.querySelector(".menu");
+const links = document.querySelectorAll("li");
+const highlight = document.querySelector(".highlight");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function () {
+  if (this.dataset.open === "close") {
+    menu.style.clipPath = "circle(100% at 50% 50%)";
+    this.dataset.open = "open";
+  } else {
+    menu.style.clipPath = "";
+    this.dataset.open = "close";
+    highlight.style = "";
+  }
+});
