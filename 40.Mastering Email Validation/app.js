cbcc5151c0e9d17688ff7a6e22fe390f0d1aef8b
pruthvi-phapal <pruthviphapal05@gmail.com>
2024-03-01 12:52:24 +0530

    40 commit

---
+++ 40.Mastering Email Validation/app.js
@@ -0,0 +1,16 @@
+let input = document.getElementById("inputbox");
+
+input.addEventListener("keydown", validate);
+
+function validate() {
+  let form = document.querySelector(".mainform");
+  let pattern = /^[^]+@[^]+\.[a-z]{2,3}$/;
+
+  if (input.value.match(pattern)) {
+    form.classList.add("valid");
+    form.classList.remove("invalid");
+  } else {
+    form.classList.add("invalid");
+    form.classList.remove("valid");
+  }
+}
